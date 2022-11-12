e2d7d765a9742283d35bca01d948ecb0b183a953
nullscc <nullscc@qq.com>
2022-11-12 11:10:21 +0800

    all

---
+++ egs2/librispeech100_fusion/asr1/run_fbank.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set="train_clean_100"
+valid_set="dev_clean"
+test_sets="test_clean"
+
+asr_tag=fbank
+asr_config=conf/train_fbank.yaml
+inference_config=conf/decode_asr.yaml
+
+./asr.sh \
+    --skip_data_prep false \
+    --skip_train false \
+    --skip_eval false \
+    --lang en \
+    --ngpu 2 \
+    --nj 12 \
+    --inference_nj 12 \
+    --nbpe 5000 \
+    --max_wav_duration 30 \
+    --audio_format "wav" \
+    --feats_type raw \
+    --use_lm false \
+    --asr_tag "${asr_tag}" \
+    --asr_config "${asr_config}" \
+    --inference_config "${inference_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --lm_train_text "data/${train_set}/text" \
+    --bpe_train_text "data/${train_set}/text" "$@"
